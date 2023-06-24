4c6c8e314945bd0e41b8d8eafcead370c19efc3c
Y <hwiyul_g@daum.net>
2023-06-24 14:19:59 +0900

    Add : Gradle에 data binding true로 설정 및 MainActivity의 databinding

--- app/src/main/java/com/example/countapp_livedata_android/View/MainActivity.kt
+++ app/src/main/java/com/example/countapp_livedata_android/View/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.countapp_livedata_android.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.countapp_livedata_android.R
+import com.example.countapp_livedata_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // data binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
